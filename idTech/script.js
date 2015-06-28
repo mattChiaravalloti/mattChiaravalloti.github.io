dab2729951fbd23a24e2549a8cf45faf1825544a
unknown <mattch@seas.upenn.edu>
2015-06-28 19:34:02 -0400

    id Tech stuff

---
+++ idTech/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    // do not write above this line
+    
+    $("#example-clickable").on("click", function () {
+        $("#hidden-example").toggle();
+    });
+    
+    // do not write below this line
+});
