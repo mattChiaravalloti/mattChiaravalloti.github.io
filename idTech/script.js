02c41b8063d4e81bca0af8b73da503c6bfafb755
unknown <mattch@seas.upenn.edu>
2015-06-28 22:39:28 -0400

    remove id stuff

--- idTech/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    // do not write above this line
-    
-    $("#example-clickable").on("click", function () {
-        $("#hidden-example").toggle();
-    });
-    
-    // do not write below this line
-});
